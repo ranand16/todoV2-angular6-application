2f82379d64330f01fbeb65525b25cd3a1dddce81
ranand16 <ranand16@gmail.com>
2019-02-25 08:37:32 +0530

    Created new project in angular 6 for todoV2 nodejs server

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todoV2-angular6-application';
+}
