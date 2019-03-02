365f4c036878cc7d3524677f7b5af20746218f3a
ranand16 <ranand16@gmail.com>
2019-03-02 14:21:29 +0530

    Added signin, signup, api-service
    Added
    * signup component
    * signin component
    * API service for user and todo

---
+++ src/app/api-service.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+/**
+ * @author <Rishabh Anand>
+ * @email <ranand16@gmail.com>
+ */
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServiceService {
+
+  constructor(private httpClient: HttpClient) { }
+  // user service functions
+  login(model: any){
+    return this.httpClient.post('https://todoapi-v2.herokuapp.com/api/login', model);
+  }
+  signup(model: any){
+    return this.httpClient.post('https://todoapi-v2.herokuapp.com/api/signup', model);
+  }
+  getUsers(){
+    return this.httpClient.get('https://todoapi-v2.herokuapp.com/api/users');
+  }
+  editUser(userId:any, model: any){
+    return this.httpClient.get('https://todoapi-v2.herokuapp.com/api/editUser/:'+userId, model);
+  }
+  getUser(model: any){
+    return this.httpClient.get('https://todoapi-v2.herokuapp.com/api/user', model);
+  }
+
+  // todo api services
+  newTask(model: any){
+    return this.httpClient.patch('https://todoapi-v2.herokuapp.com/api/', model);
+  }
+  getTasks(model: any){
+    return this.httpClient.get('https://todoapi-v2.herokuapp.com/api/', model);
+  }
+  editTask(userId: any, todoId: any, model: any){
+    return this.httpClient.patch('https://todoapi-v2.herokuapp.com/api/:'+userId+'/:'+todoId, model);
+  }
+  deleteTask(todoId: any, model: any){
+    return this.httpClient.patch('https://todoapi-v2.herokuapp.com/api/:'+todoId, model);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule
