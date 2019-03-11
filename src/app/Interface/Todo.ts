0e0da5599f8acc78ce466a1e3b12a35ea0466433
ranand16 <ranand16@gmail.com>
2019-03-11 22:50:19 +0530

    Added todo Interface and checking for its working 1

--- src/app/Interface/Todo.ts
+++ src/app/Interface/Todo.ts
@@ -1,3 +1,5 @@
 export interface Todo{
-    
-}
+    task : String;
+    isCompleted: Boolean;
+    isEditing: Boolean;
+};
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiServiceService } from '../api-service.service';
-
+import { Todo } from '../Interface/Todo';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,10 +8,17 @@ import { ApiServiceService } from '../api-service.service';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private todoServ: ApiServiceService) { }
-  
-  ngOnInit() {
+  constructor(private todoServ: ApiServiceService) { 
+  }
+  todoList: Todo[];
+  model = {};
 
+  ngOnInit() {
+    // here we will get all the tasks from server and display it on the dashboard
+    this.todoServ.getTasks(this.model).subscribe((data)=>{
+      console.log(data);
+      // this.todoList.push(data);
+    });
   }
 
   logout(){
