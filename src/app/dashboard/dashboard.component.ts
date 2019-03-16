807b1f1edde0b5187362dd636e745214490166b7
ranand16 <ranand16@gmail.com>
2019-03-16 19:46:24 +0530

    ui updates

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,7 +12,6 @@ export class DashboardComponent implements OnInit {
 
   constructor(private todoServ: ApiServiceService, private router: Router) { 
   }
-  todo: any = '';
   todoList: any[]= [];
   model = {};
 
@@ -20,9 +19,10 @@ export class DashboardComponent implements OnInit {
     if(this.isLoggedIn){
       // here we will get all the tasks from server and display it on the dashboard
       this.todoServ.getTasks().subscribe((data)=>{
-        console.log(data);
-        this.todoList.push(data);
-
+        console.log(data['todos'][0]);
+        if(data['todos'] != null){
+          this.todoList.push(data['todos'][0]);
+        }
       });
     } else{
       this.router.navigate(['/signin']); 
